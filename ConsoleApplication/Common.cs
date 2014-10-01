1f42a43eebc59750a0d602235a025c0852c27313
Fazzani <heni.fazzani@leetchi.com>
2014-10-01 17:09:24 +0200

    HFA: Création de la solution.

---
+++ ConsoleApplication/Common.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SqlServer.Server;
+using Rx = System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Common
+    {
+        /// <summary>
+        /// Get Pattern of rest Url
+        /// ex: /get/35/all => /get/{0}/all
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetPatternUrl(string url, char sep = '/', string wilcard="{0}")
+        {
+            foreach (var chaine in url.Split(sep))
+            {
+                int Num;
+                if (int.TryParse(chaine, out Num))
+                {
+                    url = url.Replace(chaine, wilcard);
+                }
+
+            }
+            //var qariRegex = new Rx.Regex(@"^([a-z]*/)*([0-9]*)/?(.*)", Rx.RegexOptions.IgnoreCase);
+            //Rx.MatchCollection mc = qariRegex.Matches(url);
+            //if (mc[0].Success && !string.IsNullOrEmpty( mc[0].Groups[2].ToString()))
+            //    return url.Replace(mc[0].Groups[2].ToString(), "{0}");
+            return url;
+        }
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new[]{
+                "Get/3","put/test/25","Get/mm","user/22/page/5"
+            };
+
+            foreach (var s in t)
+            {
+                Console.WriteLine(string.Format("traitement de la chaine {0}", s));
+                Console.WriteLine(Common.GetPatternUrl(s));
+            }
+            Console.ReadKey();
+        }
+    }
+}
