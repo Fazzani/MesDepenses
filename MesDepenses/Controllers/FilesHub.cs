898f26d4da22ba1e59289bc101537e59f6c14fbf
Fazzani <heni.fazzani@leetchi.com>
2014-10-31 23:46:02 +0100

    HFA: Add Paraller functions

--- MesDepenses/Controllers/FilesHub.cs
+++ MesDepenses/Controllers/FilesHub.cs
@@ -22,25 +22,28 @@ namespace MesDepenses.Controllers
         public async void Lancer()
         {
             string search = "jquery";
-            var path = (System.Web.HttpContext.Current == null)
+            var path = (HttpContext.Current == null)
                    ? System.Web.Hosting.HostingEnvironment.MapPath("~/")
-                   : System.Web.HttpContext.Current.Server.MapPath("~/");
+                   : HttpContext.Current.Server.MapPath("~/");
             var dirs = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories).ToList();
             _cancellationTokenSource = new CancellationTokenSource();
             var countFiles = dirs.Select(x => Directory.EnumerateFiles(x, "*", SearchOption.TopDirectoryOnly).Count()).Sum(x => x);
             Stopwatch sp = new Stopwatch();
             sp.Start();
+            ParallelOptions parallelOptions = new ParallelOptions { CancellationToken = _cancellationTokenSource.Token, MaxDegreeOfParallelism = Environment.ProcessorCount };
+
             await Task.Run(async () =>
             {
                 AsyncTestModel model = new AsyncTestModel { DataDictionary = new Dictionary<string, bool>(), RealCount = countFiles };
                 try
                 {
-                    foreach (var dir in dirs)
+                    Parallel.ForEach(dirs, parallelOptions, dir =>
                     {
                         _cancellationTokenSource.Token.ThrowIfCancellationRequested();
 
                         var files = Directory.EnumerateFiles(dir, "*", SearchOption.TopDirectoryOnly);
-                        foreach (var file in files)
+
+                        Parallel.ForEach(files, parallelOptions, async file =>
                         {
                             StreamReader myFile = new StreamReader(file);
 
@@ -48,7 +51,6 @@ namespace MesDepenses.Controllers
                             {
                                 string myString = await myFile.ReadToEndAsync();
                                 Clients.All.addFile(file, myString.Contains(search));
-                                //model.DataDictionary.Add(file, myString.Contains(search));
                                 myFile.Close();
                             }
                             catch (Exception)
@@ -60,16 +62,13 @@ namespace MesDepenses.Controllers
                                 myFile.Close();
                             }
 
-                        }
-                    }
+                        });
+                    });
                     Clients.All.end(sp.Elapsed.ToString(), countFiles);
-                    //return Json(model, JsonRequestBehavior.AllowGet);
                 }
                 catch (Exception ex)
                 {
-                    //UpdateAsyncTestModel(sp, model);
                     model.ErrorMessage = ex.Message;
-                    //return Json(model, JsonRequestBehavior.AllowGet);
                 }
                 finally
                 {
@@ -78,13 +77,6 @@ namespace MesDepenses.Controllers
             }, _cancellationTokenSource.Token);
 
         }
-
-        //private static void UpdateAsyncTestModel(Stopwatch sp, AsyncTestModel model)
-        //{
-        //    sp.Stop();
-        //    model.ElapsedTime = sp.Elapsed.ToString();
-        //    model.Count = model.DataDictionary.Count;
-        //}
 
         public async void Arreter()
         {
