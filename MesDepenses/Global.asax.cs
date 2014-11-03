f1833c6f73aa45e4230e2479bae6d2c40c03de61
Fazzani <heni.fazzani@leetchi.com>
2014-11-03 21:41:47 +0100

    HFA: Add log4net

--- MesDepenses/Global.asax.cs
+++ MesDepenses/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
+using log4net.Config;
 
 namespace MesDepenses
 {
@@ -14,10 +16,13 @@ namespace MesDepenses
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILog _log = LogManager.GetLogger("MesDepenses");
+
         protected void Application_Start()
         {
+            XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
-
+            _log.Info("Application Start....");
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
