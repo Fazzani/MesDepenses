210ca33cfa1c735a6b4557cea6e561f276178a9b
Fazzani <heni.fazzani@leetchi.com>
2015-01-04 23:08:38 +0100

    HFA

---
+++ VSXbmcViewLog/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Heni.VSXbmcViewLog
+{
+    static class GuidList
+    {
+        public const string guidVSXbmcViewLogPkgString = "157a73e5-2b99-44f4-a1a8-107e820ef061";
+        public const string guidVSXbmcViewLogCmdSetString = "03e90fed-1a1f-4982-b391-00ddf617f956";
+
+        public static readonly Guid guidVSXbmcViewLogCmdSet = new Guid(guidVSXbmcViewLogCmdSetString);
+    };
+}
---
+++ VSXbmcViewLog/LogWatcherEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heni.VSXbmcViewLog
+{
+    class LogWatcherEventArgs
+    {
+        public string Contents;
+        public LogWatcherEventArgs(string contents)
+        {
+            Contents = contents;
+        }
+    }
+}
