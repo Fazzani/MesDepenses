675f635d2ac4eff09da316aef3ce96ff24805191
Fazzani <heni.fazzani@leetchi.com>
2015-01-29 13:03:48 +0100

    HFA: Refac

--- XBMCPluginData/Controllers/ScraperController.cs
+++ XBMCPluginData/Controllers/ScraperController.cs
@@ -16,134 +16,119 @@ using XBMCPluginData.Services.Scrapers.OmgTorrent;
 
 namespace XBMCPluginData.Controllers
 {
-  [System.Web.Http.RoutePrefix("scraper")]
-  [System.Web.Http.Route("{action=index}")]
-  public class ScraperController : ApiController
-  {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="site"></param>
-    /// <param name="path"></param>
-    /// <returns></returns>
-    [System.Web.Http.Route("index/{site}/{path}", Order = 1)]
-    [System.Web.Http.HttpGet]
-    public IEnumerable<Item> Index(string site, string path = "")
+    [System.Web.Http.RoutePrefix("scraper")]
+    [System.Web.Http.Route("{action=index}")]
+    public class ScraperController : ApiController
     {
-      if (site == "omg")
-      {
-        var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
-        return scraperService.ListTorrents(null);
-      }
-      return null;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="site"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        [System.Web.Http.Route("index/{site}/{path}", Order = 1)]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Item> Index(string site, string path = "")
+        {
+            if (site != "omg") return null;
+            var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
+            return scraperService.ListTorrents(null);
+        }
+
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <param name="site"></param>
+        /// <param name="query"></param>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        [System.Web.Http.Route("{site}/search/{query}/{page}", Order = 2)]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Item> Index(string site, string query = "", int page = 0)
+        {
+            if (site != "omg") return null;
+            var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
+            return scraperService.Search(query, page);
+        }
+
+        /// <summary>
+        /// GET ex: http://localhost:1307/scraper/omg/series/Elementary/2/1
+        /// </summary>
+        /// <param name="site"></param>
+        /// <param name="serieName"></param>
+        /// <param name="serieId"></param>
+        /// <param name="saisonNumber"></param>
+        /// <returns></returns>
+        [System.Web.Http.Route("{site}/series_all/{serieName}/{serieId:int}/{saisonNumber:int}", Order = 3)]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Item> Index(string site, string serieName, int serieId, int saisonNumber)
+        {
+            if (site != "omg") return null;
+            var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
+            return scraperService.GetSaison(serieName, serieId, saisonNumber);
+        }
+
+        /// <summary>
+        /// List
+        /// </summary>
+        /// <param name="site"></param>
+        /// <param name="formatfilters"></param>
+        /// <param name="path"></param>
+        /// <param name="page"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [System.Web.Http.Route("{site}/{path}/{page:int:min(1)?}/{orderBy:int?}/{order:int?}", Order = 4)]
+        [OutputCache(Duration = 10000, VaryByParam = "site;path;category;page;orderBy")]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Item> List(string site, [FromUri]List<string> formatfilters, string path = "", int page = 1, OmgScraperService.OrderByEnum orderBy = OmgScraperService.OrderByEnum.DateAjout, OmgScraperService.OrderEnum order = OmgScraperService.OrderEnum.Desc)
+        {
+            //http://localhost:1307/scraper/omg/films/action/1/1/1
+            //http://localhost:1307/scraper/omg/films/1/1/1
+            //http://localhost:1307/scraper/omg/series-episodes/1/1/1
+            if (site != "omg") return null;
+            var filters = formatfilters.ToDictionary(x => x, y => "id_cat[]");
+            var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
+            if (path.Equals("series"))
+                return scraperService.ListTorrents(filters, string.Empty, path, page, orderBy, order, OmgScraperService.TypeExtractEnum.SerieBlock);
+
+            return scraperService.ListTorrents(filters, string.Empty, path, page, orderBy, order);
+        }
+
+        /// <summary>
+        /// List
+        /// </summary>
+        /// <param name="site"></param>
+        /// <param name="category"></param>
+        /// <param name="formatfilters"></param>
+        /// <param name="path"></param>
+        /// <param name="page"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [System.Web.Http.Route("{site}/{path}/{category}/{page:int:min(1)?}/{orderBy:int?}/{order:int?}", Order = 5)]
+        [OutputCache(Duration = 10000, VaryByParam = "site;path;category;page;orderBy")]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Item> List(string site, string category, [FromUri]List<string> formatfilters, string path = "", int page = 1, OmgScraperService.OrderByEnum orderBy = OmgScraperService.OrderByEnum.DateAjout, OmgScraperService.OrderEnum order = OmgScraperService.OrderEnum.Desc)
+        {
+            //http://localhost:1307/scraper/omg/films/action/1/1/1
+            //http://localhost:1307/scraper/omg/films/1/1/1
+            //http://localhost:1307/scraper/omg/series-episodes/1/1/1
+            if (site != "omg") return null;
+            var filters = formatfilters.ToDictionary(x => "id_cat[]", y => y);
+            var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
+            if (path.Equals("series"))
+                return scraperService.ListTorrents(filters, category, path, page, orderBy, order, OmgScraperService.TypeExtractEnum.SerieBlock);
+
+            return scraperService.ListTorrents(filters, category, path, page, orderBy, order);
+        }
+
+        //[System.Web.Http.Route("index")]
+        //[System.Web.Http.HttpGet]
+        //public async Task<IEnumerable<KeyValuePair<string, string>>> TestDecodeFile()
+        //{
+        //  return await Tools.GetTorrentInfoAsync("http://www.omgtorrent.com/clic_dl.php?id=18911");
+        //}
+
     }
-
-    /// <summary>
-    /// Index
-    /// </summary>
-    /// <param name="site"></param>
-    /// <param name="query"></param>
-    /// <param name="page"></param>
-    /// <returns></returns>
-    [System.Web.Http.Route("{site}/search/{query}/{page}", Order = 2)]
-    [System.Web.Http.HttpGet]
-    public IEnumerable<Item> Index(string site, string query = "", int page = 0)
-    {
-      if (site == "omg")
-      {
-        var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
-        return scraperService.Search(query, page);
-      }
-      return null;
-    }
-
-    /// <summary>
-    /// GET ex: http://localhost:1307/scraper/omg/series/Elementary/2/1
-    /// </summary>
-    /// <param name="site"></param>
-    /// <param name="serieName"></param>
-    /// <param name="serieId"></param>
-    /// <param name="saisonNumber"></param>
-    /// <returns></returns>
-    [System.Web.Http.Route("{site}/series_all/{serieName}/{serieId:int}/{saisonNumber:int}", Order = 3)]
-    [System.Web.Http.HttpGet]
-    public IEnumerable<Item> Index(string site, string serieName, int serieId, int saisonNumber)
-    {
-      if (site == "omg")
-      {
-        var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
-        return scraperService.GetSaison(serieName, serieId, saisonNumber);
-      }
-      return null;
-    }
-
-    /// <summary>
-    /// List
-    /// </summary>
-    /// <param name="site"></param>
-    /// <param name="formatfilters"></param>
-    /// <param name="path"></param>
-    /// <param name="page"></param>
-    /// <param name="orderBy"></param>
-    /// <param name="order"></param>
-    /// <returns></returns>
-    [System.Web.Http.Route("{site}/{path}/{page:int:min(1)?}/{orderBy:int?}/{order:int?}", Order = 4)]
-    [OutputCache(Duration = 10000, VaryByParam = "site;path;category;page;orderBy")]
-    [System.Web.Http.HttpGet]
-    public IEnumerable<Item> List(string site, [FromUri]List<string> formatfilters, string path = "", int page = 1, OmgScraperService.OrderByEnum orderBy = OmgScraperService.OrderByEnum.DateAjout, OmgScraperService.OrderEnum order = OmgScraperService.OrderEnum.Desc)
-    {
-      //http://localhost:1307/scraper/omg/films/action/1/1/1
-      //http://localhost:1307/scraper/omg/films/1/1/1
-      //http://localhost:1307/scraper/omg/series-episodes/1/1/1
-      var filters = formatfilters.ToDictionary(x => x, y => "id_cat[]");
-      if (site == "omg")
-      {
-        var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
-        if (path.Equals("series"))
-          return scraperService.ListTorrents(filters, string.Empty, path, page, orderBy, order, OmgScraperService.TypeExtractEnum.SerieBlock);
-
-        return scraperService.ListTorrents(filters, string.Empty, path, page, orderBy, order);
-      }
-      return null;
-    }
-
-    /// <summary>
-    /// List
-    /// </summary>
-    /// <param name="site"></param>
-    /// <param name="category"></param>
-    /// <param name="formatfilters"></param>
-    /// <param name="path"></param>
-    /// <param name="page"></param>
-    /// <param name="orderBy"></param>
-    /// <param name="order"></param>
-    /// <returns></returns>
-    [System.Web.Http.Route("{site}/{path}/{category}/{page:int:min(1)?}/{orderBy:int?}/{order:int?}", Order = 5)]
-    [OutputCache(Duration = 10000, VaryByParam = "site;path;category;page;orderBy")]
-    [System.Web.Http.HttpGet]
-    public IEnumerable<Item> List(string site, string category, [FromUri]List<string> formatfilters, string path = "", int page = 1, OmgScraperService.OrderByEnum orderBy = OmgScraperService.OrderByEnum.DateAjout, OmgScraperService.OrderEnum order = OmgScraperService.OrderEnum.Desc)
-    {
-      //http://localhost:1307/scraper/omg/films/action/1/1/1
-      //http://localhost:1307/scraper/omg/films/1/1/1
-      //http://localhost:1307/scraper/omg/series-episodes/1/1/1
-      if (site == "omg")
-      {
-        var filters = formatfilters.ToDictionary(x => "id_cat[]", y => y);
-        var scraperService = new OmgScraperService(ConfigurationManager.AppSettings[site]);
-        if (path.Equals("series"))
-          return scraperService.ListTorrents(filters, category, path, page, orderBy, order, OmgScraperService.TypeExtractEnum.SerieBlock);
-
-        return scraperService.ListTorrents(filters, category, path, page, orderBy, order);
-      }
-      return null;
-    }
-
-    //[System.Web.Http.Route("index")]
-    //[System.Web.Http.HttpGet]
-    //public async Task<IEnumerable<KeyValuePair<string, string>>> TestDecodeFile()
-    //{
-    //  return await Tools.GetTorrentInfoAsync("http://www.omgtorrent.com/clic_dl.php?id=18911");
-    //}
-
-  }
 }
