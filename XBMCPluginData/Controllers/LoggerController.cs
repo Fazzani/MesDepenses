f103518397a891ced7dfdc565447688ce0d661e1
Fazzani <heni.fazzani@leetchi.com>
2015-01-30 14:53:37 +0100

    HFA: Online Logger Service

---
+++ XBMCPluginData/Controllers/LoggerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HtmlAgilityPack;
+
+namespace XBMCPluginData.Controllers
+{
+    [System.Web.Http.RoutePrefix("logger")]
+    [System.Web.Http.Route("{action=post}")]
+    public class LoggerController : ApiController
+    {
+        /// <summary>
+        /// POST api/values
+        /// </summary>
+        /// <param name="value"></param>
+        public void Post([FromBody]string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
--- XBMCPluginData/Startup.cs
+++ XBMCPluginData/Startup.cs
@@ -13,6 +13,8 @@ namespace XBMCPluginData
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
+
     }
 }
