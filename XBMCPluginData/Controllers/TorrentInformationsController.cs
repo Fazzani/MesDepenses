e972d3079d114790bc27a4db74d4792cfa0087a7
Fazzani <heni.fazzani@leetchi.com>
2015-02-02 23:07:36 +0100

    HFA

--- XBMCPluginData/Controllers/TorrentInformationsController.cs
+++ XBMCPluginData/Controllers/TorrentInformationsController.cs
@@ -5,19 +5,19 @@ using XBMCPluginData.Services.Scrapers.OmgTorrent;
 
 namespace XBMCPluginData.Controllers
 {
-    [RoutePrefix("torrent")]
-    public class TorrentInformationsController : ApiController
-    {
-        /// <summary>
-        /// GetTorrentInfos ex: http://localhost:1307/torrent/torrentLink
-        /// </summary>
-        /// <param name="torrentLink"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("Get")]
-        public IEnumerable<KeyValuePair<string, InfoFromTorrentName>> Get([FromBody]string torrentLink)
-        {
-            return OmgScraperService.GetTorrentInfos(torrentLink);
-        }
-    }
+  [RoutePrefix("torrent")]
+  public class TorrentInformationsController : ApiController
+  {
+    /// <summary>
+    /// GetTorrentInfos ex: http://localhost:1307/torrent/torrentLink
+    /// </summary>
+    /// <param name="torrentLink"></param>
+    /// <returns></returns>
+    //[HttpPost]
+    //[Route("Get")]
+    //public InfoFromTorrentName Get([FromBody]string torrentLink)
+    //{
+    // // return TorrentHelper.GetTorrentInfoAsync(torrentLink);
+    //}
+  }
 }
--- XBMCPluginData/Helpers/TorrentHelper.cs
+++ XBMCPluginData/Helpers/TorrentHelper.cs
@@ -8,126 +8,95 @@ using System.Net;
 using System.Net.Torrent;
 using System.Net.Torrent.BEncode;
 using System.Threading.Tasks;
-using FluentSharp.CoreLib;
 
 namespace XBMCPluginData.Helpers
 {
-    public class TorrentHelper
+  public class TorrentHelper
+  {
+    //Revenge S04E05 FASTSUB VOSTFR HDTV XviD
+    private const string tvRegex = @"^(.*)S(?<season>\d{1,2})E(?<episode>\d{1,2})(.*)$";
+    //10 bonnes raisons de te larguer FRENCH DVDRip XviD-NoTag
+    private const string movieRegex = @"^(.*?)(STV|FRENCH|dvdrip|(?:XviD-?(?:.*))|cd[0-9]|dvdscr|brrip|divx|[\{\(\[]?[0-9]{4}).*$";
+    private const string dateRegex = @"^\w* ([0-9]{4} )+.*$";
+
+    /// <summary>
+    /// Get Tv série Info From Torrent Name
+    /// </summary>
+    /// <param name="torrentName"></param>
+    /// <param name="saisonLabel"></param>
+    /// <returns></returns>
+    public static InfoFromTorrentName GetTvInfoFromTorrentName(string torrentName, string saisonLabel = "")
     {
-        //Revenge S04E05 FASTSUB VOSTFR HDTV XviD
-        private const string tvRegex = @"^(.*)S(?<season>\d{1,2})E(?<episode>\d{1,2})(.*)$";
-        //10 bonnes raisons de te larguer FRENCH DVDRip XviD-NoTag
-        private const string movieRegex = @"^(.*?)(STV|FRENCH|dvdrip|(?:XviD-?(?:.*))|cd[0-9]|dvdscr|brrip|divx|[\{\(\[]?[0-9]{4}).*$";
-        private const string dateRegex = @"^\w* ([0-9]{4} )+.*$";
-
-        /// <summary>
-        /// Get Tv série Info From Torrent Name
-        /// </summary>
-        /// <param name="torrentName"></param>
-        /// <param name="saisonLabel"></param>
-        /// <returns></returns>
-        public static InfoFromTorrentName GetTvInfoFromTorrentName(string torrentName, string saisonLabel = "")
+      Regex reg = new Regex(tvRegex);
+      var match = reg.Match(torrentName.Replace(".", " ").Replace("-", " "));
+      if (match.Success)
+      {
+        return new InfoFromTorrentName
         {
-            Regex reg = new Regex(tvRegex);
-            var match = reg.Match(torrentName.Replace(".", " ").Replace("-", " "));
-            if (match.Success)
-            {
-                return new InfoFromTorrentName(match.Groups[1].Value)
-                {
-                    EpisodeNumber = Convert.ToInt32(match.Groups["episode"].Value),
-                    Quality = match.Groups["episode"].Value,
-                    SaisonNumber = Convert.ToInt32(match.Groups["season"].Value),
-                    SaisonLabel = saisonLabel
-                };
-            }
-            return new InfoFromTorrentName(torrentName);
-        }
-
-        /// <summary>
-        /// Get Tv série Info From Torrent Name
-        /// </summary>
-        /// <param name="torrentName"></param>
-        /// <returns></returns>
-        public static InfoFromTorrentName GetMovieInfoFromTorrentName(string torrentName)
-        {
-            Regex reg = new Regex(movieRegex);
-            var matchLabel = reg.Match(torrentName);
-            if (matchLabel.Success)
-            {
-                reg = new Regex(dateRegex);
-                var matchDate = reg.Match(torrentName);
-                if (!matchDate.Success)
-                    return new InfoFromTorrentName(matchLabel.Groups[1].Value);
-                return new InfoFromTorrentName(matchLabel.Groups[1].Value)
-                {
-                    Year = Int32.Parse(matchDate.Groups[1].Value)
-                };
-            }
-            return new InfoFromTorrentName(torrentName);
-        }
-
-        /// <summary>
-        /// Get Torrent info
-        /// </summary>
-        /// <param name="uri"></param>
-        /// <returns></returns>
-        public static InfoFromTorrentName GetTorrentInfoAsync(string uri)
-        {
-            WebClient webClient = new WebClient();
-            webClient.Headers.Add("User-Agent",
-                "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
-            var bytes = webClient.DownloadData(uri);
-
-            var res = BencodingUtils.Decode(bytes) as BDict;
-            var infos = res.SingleOrDefault(x => x.Key == "info").Value as BDict;
-            var name = infos.FirstOrDefault(x => x.Key == "name").Value.ToString();
-            if (infos.hasKey("files"))
-            {
-                var files =
-                    (infos.FirstOrDefault(x => x.Key == "files").Value as BList).Select(x => x)
-                        .Cast<BDict>()
-                        .Select(
-                            x =>
-                                new TorrentFileInfo((x.FirstOrDefault(k => k.Key == "path").Value as BList)[0].ToString())
-                                {
-                                    Size = x.FirstOrDefault(k => k.Key == "length").Value.ToString()
-                                });
-
-
-                return new InfoFromTorrentName(name) { TorrentFilesInfos = files };
-            }
-            return new InfoFromTorrentName(name);
-        }
+          EpisodeNumber = Convert.ToInt32(match.Groups["episode"].Value),
+          Label = match.Groups[1].Value,
+          Quality = match.Groups["episode"].Value,
+          SaisonNumber = Convert.ToInt32(match.Groups["season"].Value),
+          SaisonLabel = saisonLabel
+        };
+      }
+      return new InfoFromTorrentName { Label = torrentName };
     }
 
-    public class InfoFromTorrentName
+    /// <summary>
+    /// Get Tv série Info From Torrent Name
+    /// </summary>
+    /// <param name="torrentName"></param>
+    /// <returns></returns>
+    public static InfoFromTorrentName GetMovieInfoFromTorrentName(string torrentName)
     {
-        public InfoFromTorrentName(string name)
+      Regex reg = new Regex(movieRegex);
+      var matchLabel = reg.Match(torrentName);
+      if (matchLabel.Success)
+      {
+        reg = new Regex(dateRegex);
+        var matchDate = reg.Match(torrentName);
+        if (!matchDate.Success)
+          return new InfoFromTorrentName
+          {
+            Label = matchLabel.Groups[1].Value,
+          };
+        return new InfoFromTorrentName
         {
-            TorrentFileInfo = new TorrentFileInfo(name);
-        }
-        public TorrentFileInfo TorrentFileInfo { get; set; }
-        public int SaisonNumber { get; set; }
-        public int EpisodeNumber { get; set; }
-        public string Quality { get; set; }
-        public int Year { get; set; }
-        public string SaisonLabel { get; set; }
-        /// <summary>
-        /// TorrentFileInfo
-        /// </summary>
-        public IEnumerable<TorrentFileInfo> TorrentFilesInfos { get; set; }
-
+          Label = matchLabel.Groups[1].Value,
+          Year = Int32.Parse(matchDate.Groups[1].Value)
+        };
+      }
+      return new InfoFromTorrentName { Label = torrentName };
     }
 
-    public class TorrentFileInfo
+    /// <summary>
+    /// Get Torrent info
+    /// </summary>
+    /// <param name="uri"></param>
+    /// <returns></returns>
+    public static Tuple<string, IEnumerable<KeyValuePair<string, string>>> GetTorrentInfoAsync(string uri)
     {
-        public TorrentFileInfo(string name)
-        {
-            Name = name;
-        }
-        public string Name { get; set; }
-        public string Size { get; set; }
+      WebClient webClient = new WebClient();
+      webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
+      var bytes = webClient.DownloadData(uri);
 
+      var res = BencodingUtils.Decode(bytes) as BDict;
+      var infos = res.SingleOrDefault(x => x.Key == "info").Value as BDict;
+      var name = infos.FirstOrDefault(x => x.Key == "name").Value.ToString();
+      var files = (infos.FirstOrDefault().Value as BList).Select(x => x).Cast<BDict>().Select(x => new System.Collections.Generic.KeyValuePair<string, string>((x.FirstOrDefault(k => k.Key == "path").Value as BList)[0].ToString(), x.FirstOrDefault(k => k.Key == "length").Value.ToString()));
+      return new Tuple<string, IEnumerable<KeyValuePair<string, string>>>(name, files);
     }
+  }
+
+  public class InfoFromTorrentName
+  {
+    public string Label { get; set; }
+    public int SaisonNumber { get; set; }
+    public int EpisodeNumber { get; set; }
+    public string Quality { get; set; }
+    public int Year { get; set; }
+    public string SaisonLabel { get; set; }
+  }
 
 }
